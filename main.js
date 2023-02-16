f8bbb1af5f99f2813bc459527640ec57419b0150
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2023-02-15 17:31:22 -0800

    added depth

--- main.js
+++ main.js
@@ -7,8 +7,24 @@ button.style.display = "none";
 img.style.display = "none";
 });
 
+// var target = document.getElementById('fse-link');
+// var tag = document.getElementById('demo');
+// var link = document.querySelector('a'); 
+// link.addEventListener('click',function() {
+// target.classList.toggle('demo')
+// });
+
 // $(document).ready(function(){
 //     $('[data-toggle="tooltip"]').tooltip();   
-//   });
+// //   });
 
+// const link = document.querySelector('a');
+// const 
+// link.addEventListener('mouseover', () => {
+//     link.classList.add('hovered');
+// });
+
+// link.addEventListener('mouseout', () => {
+//     link.classList.remove('hovered');
+// });
  
