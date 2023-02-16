17c56e0b01b5ba3e044225ca86a6e57ea4117ffc
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2023-02-15 16:35:13 -0800

    updated photo

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var tag = document.getElementById('demo');
 var button = document.querySelector('button');
+var img = document.querySelector('img')
 button.addEventListener('click', function(){
 tag.classList.toggle('demo');
 button.style.display = "none";
+img.style.display = "none";
 });
 
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
-  });
+// $(document).ready(function(){
+//     $('[data-toggle="tooltip"]').tooltip();   
+//   });
 
  
