d97e5da59ac856eb0792894601da61e79b5843ba
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2023-02-23 17:38:48 -0800

    homepage formatting final

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-var tag = document.getElementById('about');
-var button = document.querySelector('button');
-var img = document.querySelector('img');
-var h1 =document.querySelector('h1');
-var ul =document.getElementById('fun-bar');
+// var tag = document.getElementById('about');
+// var button = document.querySelector('button');
+// var img = document.querySelector('img');
+// var h1 =document.querySelector('h1');
+// var ul =document.getElementById('fun-bar');
 
 
-button.addEventListener('click', function(){
-    tag.classList.toggle('about');
-    button.style.display = "none";
-    img.style.display = "none";
-    ul.style.display = "none";
+// button.addEventListener('click', function(){
+//     tag.classList.toggle('about');
+//     button.style.display = "none";
+//     img.style.display = "none";
+//     ul.style.display = "none";
     
      
-});
+// });
+
+
+function openPopup() {
+  // Open the pop-up window
+  window.open('scrum.html', 'popup', 'width=600,height=400');
+
+  // Cancel the default behavior of the link
+  return false;
+}
+
+  
 
 
 
