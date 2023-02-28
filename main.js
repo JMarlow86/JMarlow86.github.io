62d53085c3d9eec89bf548d69cf6cd5ad09fc346
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2023-02-27 16:16:54 -0800

    added search bar, formatting broken

--- main.js
+++ main.js
@@ -4,17 +4,14 @@ let copyrightNotice = document.createElement("footer");
 copyrightNotice.innerHTML = "Copyright © " + currentYear + " Jamie Marlow. All Rights Reserved";
 document.body.appendChild(copyrightNotice);
 
-$(document).ready(function(){
-  $('#myCarousel').carousel();
-});
-
-$('.carousel').carousel({
-  interval: 6000
-})
 
 
+function search(){
+  let textToSearch = document.getElementById("text-to-search").value;
+  let forSearch = document.getElementById("forSearch");
+  textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
 
-  
+  let pattern = new RegExp(`${textToSearch}`,"gi");
 
-
-
+  forSearch.innerHTML = forSearch.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+}
